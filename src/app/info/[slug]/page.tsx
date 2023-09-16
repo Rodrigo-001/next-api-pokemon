e98fbb83fcea066717631d76e5fe7ed1df169f1d
rodrigoc01 <rodrigocamargo1234santos@gmail.com>
2023-09-15 22:26:54 -0300

    corrigido problema em Image

--- src/app/info/[slug]/page.tsx
+++ src/app/info/[slug]/page.tsx
@@ -1,29 +1,23 @@
 import Image from "next/image";
 
-
-export async function generateStaticParams(){
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-    const {results: pokemons} = await response.json();
-    return pokemons.map((pokemon: any) => ({        
-        slug: pokemon.name        
-    }));
+export async function generateStaticParams() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+    const{ results: pokemons }= await response.json();
+    return pokemons.map((pokemon: any) => ({ slug: pokemon.name}));
 }
-
-export default async function InfoDetails({params, }: {params: {slug: string};}){
+export default async function infoDetails({params,}:{ params: { slug: string};}){
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.slug}`);
     const pokemon = await response.json();
 
     return(
-    <div className="flex items-center">
-        <Image
-            src={pokemon.sprites.front_shiny}
-            width={80}
-            height={80}
-            alt={pokemon.name}
-        />
-        <h2>
-            {pokemon.name}
-        </h2>
-    </div>
-    );
+        <div className="flex items-center">
+            <Image
+                src={pokemon.sprites.front_shiny}
+                width={80}
+                height={80}
+                alt={pokemon.name}
+            />
+            <h2 className="text-2xl mb-4 font-blod">{pokemon.name}</h2>
+        </div>
+    );  
 }
